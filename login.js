2d0fa7020b7dc4063c9e8aea642a6cc4e026878a
esther <ecortereal45@gmail.com>
2024-11-07 22:23:09 -0300

    pagina js login

--- login.js
+++ login.js
@@ -14,3 +14,32 @@ function mostrar(){
         btnShowPass.classList.replace('bi-eye-slash', 'bi-eye')
     }
 }
+
+// senha correta ou incorreta
+
+
+const usuarios = [
+    { email: "erick@lasalle.com", senha: "senha123" },
+    { email: "usuario2@example.com", senha: "senha456" },
+    { email: "usuario3@example.com", senha: "senha789" }
+];
+
+/
+document.querySelector('.form1').addEventListener('submit', function(e) {
+    e.preventDefault(); // Impede o envio do formulário para não recarregar a página
+
+    const email = document.getElementById('input1').value;
+    const senha = document.getElementById('input2').value;
+
+    
+    const usuario = usuarios.find(u => u.email === email && u.senha === senha);
+
+    const mensagemDiv = document.getElementById('texto-e');
+    if (usuario) {
+        mensagemDiv.textContent = 'Login bem-sucedido!';
+        mensagemDiv.style.color = 'green';
+    } else {
+        mensagemDiv.textContent = 'Email ou senha inválidos.';
+        mensagemDiv.style.color = 'red';
+    }
+});
